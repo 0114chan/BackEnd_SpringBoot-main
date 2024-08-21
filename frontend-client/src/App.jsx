2ee2a444d4b81c3aab0119cfab98602859b76474
0114chan <0114chan@gmail.com>
2024-08-21 10:49:14 +0900

    Initial commit

---
+++ frontend-client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import HeaderComponent from "src/components/common/HeaderComponent";
+import FooterComponent from "src/components/common/FooterComponent";
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <HeaderComponent />
+                <FooterComponent />
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
